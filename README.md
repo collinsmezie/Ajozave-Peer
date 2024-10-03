41b753392edbe9ecdd3e89e69b069468e56a0146
collinsmezie <collinschimezieobinwa@gmail.com>
2024-10-03 17:17:32 +0100

    Peer Account - home & auth page redesign

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Ajozave-Peer
+# Ajozave-admin Interface
+
+Admin interface for managing sessions and users.
 
 Ajozave is a contribution platform based on the Nigerian traditional savings system - A one stop solution for users engaging in local savings.
--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -1,31 +1,3 @@
-// // src/components/BottomNavigation.js
-// import React from 'react';
-
-// const BottomNavigation = () => {
-//   const navItems = [
-//     { name: 'Home', icon: 'fas fa-home' },
-//     { name: 'Pay & Trans.', icon: 'fas fa-money-check-alt' },
-//     { name: 'Products', icon: 'fas fa-box-open' },
-//     { name: 'Cards', icon: 'fas fa-credit-card' },
-//     { name: 'Menu', icon: 'fas fa-bars' }
-//   ];
-
-//   return (
-//     <div className="fixed bottom-0 w-full bg-white shadow-md flex justify-between items-center p-4">
-//       {navItems.map((item, index) => (
-//         <div key={index} className="flex flex-col items-center space-y-1">
-//           <i className={`${item.icon} text-gray-600 text-xl`}></i>
-//           <span className="text-gray-600 text-xs">{item.name}</span>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default BottomNavigation;
-
-
-
 // src/components/BottomNavigation.js
 import React from 'react';
 
@@ -39,14 +11,11 @@ const BottomNavigation = () => {
   ];
 
   return (
-    <div className="fixed bottom-0 w-full bg-white shadow-md flex justify-between items-center p-2">
+    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-sm bg-white shadow-md flex justify-between items-center p-2">
       {navItems.map((item, index) => (
-        <div key={index} className="flex flex-col items-center space-y-1 w-1/5">
-          {/* <i className={`${item.icon} text-gray-600 text-xl`}></i> */}
-          
-          <i className={`${item.icon} ${index === 0 ? 'text-red-600': 'text-gray-600'} text-xl`}></i>
-
-          <span className="text-gray-600 text-xs">{item.name}</span>
+        <div key={index} className="flex flex-col items-center w-1/5">
+          <i className={`${item.icon} ${index === 0 ? 'text-red-600' : 'text-gray-300'} text-lg`}></i>
+          <span className={`${index === 0 ? 'text-red-600' : 'text-gray-500'} text-xs`}>{item.name}</span>
         </div>
       ))}
     </div>
@@ -54,3 +23,4 @@ const BottomNavigation = () => {
 };
 
 export default BottomNavigation;
+
